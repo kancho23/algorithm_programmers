661a74432725503c881d764647c342425a04c725
kancho23 <92067836+kancho23@users.noreply.github.com>
2022-05-30 14:29:34 +0900

    20220530

---
+++ 완주하지못한선수.py
@@ -0,0 +1,45 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def solution(participant, completion):
+    answer = ""
+    # 정렬해서 배열한다
+    participant.sort()
+    completion.sort()
+
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            answer = participant[i]
+            break
+
+    if answer == "":
+        answer = participant[-1]
+
+    return answer
+
+def solution(participant, completion):
+    hashDict = {}
+    sumHash = 0
+
+    for part in participant:
+        hashDict[hash(part)] = part
+        sumHash += hash(part)
+        #part는 사람 이름
+        #hash(part)는 그 사람한테 해당되는 숫자(value)
+
+    for comp in completion:
+        sumHash -= hash(comp)
+        #comp라는 사람이름의, 해당되는 숫자를 뺌
+
+    return hashDict[sumHash]
+    #달리지 않은 사람의 숫자는 sumHash이고,
+    #이를 다시 hashDict[]에 넣으면, 사람 이름이 튀어나온다.
+
+import collections
+def solution(participant, completion):
+    #리스트를 가지고 counter를 생성하면, key가 이름이고 value가 count인 딕셔너리 반환
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
+
